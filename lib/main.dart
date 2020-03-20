086b403c28f2611e24cfc53747d06b5598dfba8c
Romain <r.chevallier21@hotmail.fr>
2020-03-20 09:00:43 +0100

    fix unit tests

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
     children.add(
       Center(
         child: DropdownButton(
+          key: Key('Country List'),
           onChanged: (value) {
             setState(() {
               _dropDownValue = value;
