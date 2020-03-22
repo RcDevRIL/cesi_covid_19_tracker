b022b0f2e7b7cc5f77169304587bd6868c0f45e8
Romain <r.chevallier21@hotmail.fr>
2020-03-22 20:50:30 +0100

    Website v0.1-RELEASE

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,12 +5,12 @@ const RESOURCES = {
 "/assets/assets/virus.jpg": "5a890fa31e431d57d18aa604042d44af",
 "/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "024404b3e11cdb3e16a42a9e74a4dbc7",
+"/assets/LICENSE": "43d5214e0b74b83a6e69ada9172fda58",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "2e028f6f136c28ad37d5f053f2e41c65",
-"/main.dart.js": "591c20b6680470b48f82f06bada27429",
+"/main.dart.js": "36ab9b636023b044ad153a36640efa3a",
 "/manifest.json": "5ffc197ea4c71c04aac50efea46adc12"
 };
 
