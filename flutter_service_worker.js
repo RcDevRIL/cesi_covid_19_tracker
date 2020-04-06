2430141ae8e2f648d03eab155944f1ab8a31a3e5
Stark <james.71@hotmail.fr>
2020-04-06 14:44:36 +0200

    Web site v0.2-alpha

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "2e028f6f136c28ad37d5f053f2e41c65",
-"/main.dart.js": "36ab9b636023b044ad153a36640efa3a",
+"/main.dart.js": "116839bc594f2978772a6d2811922398",
 "/manifest.json": "5ffc197ea4c71c04aac50efea46adc12"
 };
 
