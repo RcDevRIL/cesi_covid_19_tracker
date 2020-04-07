8944bfdd17c3c3be6e26ca4519b55198371593e7
Romain <r.chevallier21@hotmail.fr>
2020-04-07 12:21:04 +0200

    Website v0.3-RELEASE

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const RESOURCES = {
 "assets/assets/virus.jpg": "5a890fa31e431d57d18aa604042d44af",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "3b89287cac80bea82cf6040dfbb8d589",
+"assets/LICENSE": "4fd868e70204c8c4e3b3576f5fef2d3d",
 "favicon.ico": "cd78fd2732d09d2595ba1b16b9d6a488",
 "icons/android-icon-144x144.png": "fb70f5cda7db961105feb0f09b6f2899",
 "icons/android-icon-192x192.png": "cb618464d5fe4baffb584ad6767490cd",
@@ -34,7 +34,7 @@ const RESOURCES = {
 "icons/ms-icon-70x70.png": "48f490a64da323b6fe6daa506471699b",
 "index.html": "7c005a087ba16c486930d78d526505a5",
 "/": "7c005a087ba16c486930d78d526505a5",
-"main.dart.js": "cbdea46509462aa5a5c432817215c465",
+"main.dart.js": "6003e73e0deebf568742566f6127e631",
 "manifest.json": "610bcc38a0da86239a4f9d2472cb7d67"
 };
 
