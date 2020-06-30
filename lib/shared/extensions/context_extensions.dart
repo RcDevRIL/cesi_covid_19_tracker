8aabc6d47e877a7f1a5294ba3934ca8273521b45
Romain <r.chevallier21@hotmail.fr>
2020-06-30 16:22:37 +0200

    update context_extension.dart

--- lib/shared/extensions/context_extensions.dart
+++ lib/shared/extensions/context_extensions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 /// Custom breakpoints when not using responsive_builder package
 extension SizeBreakpoint on BuildContext {
-  bool get isWatch => MediaQuery.of(this).size.width <= 300;
-  bool get isMobile => MediaQuery.of(this).size.width <= 600;
-  bool get isTablet => MediaQuery.of(this).size.width <= 900;
+  bool get isWatch => MediaQuery.of(this).size.width < 300;
+  bool get isMobile => 300 <= MediaQuery.of(this).size.width && MediaQuery.of(this).size.width <= 600;
+  bool get isTablet => 600 < MediaQuery.of(this).size.width && MediaQuery.of(this).size.width <= 900;
 }
